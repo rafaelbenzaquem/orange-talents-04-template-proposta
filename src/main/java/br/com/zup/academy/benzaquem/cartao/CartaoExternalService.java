7551abff12066c3537fb6a6f918c3e8793b6185a
Rafael Benzaquem Neto <rafael.neto@outlook.com>
2021-05-12 10:42:48 -0300

    Feature para acompanhar uma proposta e correções de feature de associar cartão a proposta e criar proposta

--- src/main/java/br/com/zup/academy/benzaquem/cartao/CartaoExternalService.java
+++ src/main/java/br/com/zup/academy/benzaquem/cartao/CartaoExternalService.java
@@ -1,12 +1,13 @@
 package br.com.zup.academy.benzaquem.cartao;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(url = "http://localhost:8888/api/cartoes", name = "cartoes")
 public interface CartaoExternalService {
 
     @PostMapping(consumes = "application/json")
-    IdCartaoResponse recuperarDadosCartao(SolicitanteCartaoRequest request);
+    IdCartaoResponse recuperarDadosCartao(@PathVariable Long idProposta);
 
 }
